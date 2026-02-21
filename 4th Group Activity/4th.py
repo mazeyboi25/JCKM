94f8c1e15b37438d4bff18039305060eb3280b69
Kyle Ochavillo <ochavillo.kylebenedict25@gmail.com>
2026-02-21 09:23:34 +0000

    Implemented sequential and multiprocessing parallel exec

--- 4th Group Activity/4th.py
+++ 4th Group Activity/4th.py
@@ -1,4 +1,6 @@
 import random
+from multiprocessing import Pool, cpu_count
+from grader import grader_paper
 
 def generate_papers(num_papers, num_questions):
     return {
@@ -10,3 +12,14 @@ def grade_paper(paper):
     for answer in paper:
         total += answer
     return total
+
+def sequential_grading(papers):
+    results = ()
+    for paper in papers:
+        results.append(grade_paper(paper))
+    return results
+
+def parallel_grading(papres):
+    with Pool(cpu_count()) as pool:
+        results = pool.map(grade_paper, papers)
+    return results
